c9655a835bdb5c9b3431ab96dfc3250369871cba
danielchvezs <danielechs@hotmail.com>
2022-11-17 17:57:10 -0500

    Ejercicios 1 y 2 completados

--- 01.js
+++ 01.js
@@ -11,7 +11,12 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
-
+  var arrayEnteros = [];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] % 1 == 0)
+    arrayEnteros.push(array[i]);  
+ }
+  return arrayEnteros; 
 }
 
 // No modifiques nada debajo de esta linea //
